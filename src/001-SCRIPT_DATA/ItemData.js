5045e70991d1dbb3d519767a1395316a3309c6e4
Mystery Game Dev <jjm152@hotmail.com>
2018-05-12 15:15:06 +1000

    Added some gothic lolita clothing, contributed by 'littleslavegirl'

--- src/001-SCRIPT_DATA/ItemData.js
+++ src/001-SCRIPT_DATA/ItemData.js
@@ -567,6 +567,61 @@ window.App.Data.Clothes = {
                 "Slot": "Butt", "Restrict": ["Butt"], "Color": "black", "Price": 200, "Style": "RARE", "Type" : "ACCESSORY",
                 "WearEffect": {"Perversion": [0, 10, 70], "AssFucking" : [10, 10, 80]}, Category : [ "BDSM" ]
             },
+// Gothic Lolita Style, contributed by 'littleslavegirl' @ wwww.tfgamessite.com Forums
+            "black flower hairpin": { // 12
+                "Name": "red and black flower hair pin", "ShortDesc": "a red and {COLOR} flower",
+                "LongDesc": "A black rose that adorns your hair, a trio of black ribbons hang down to which are attached 3 blood red stars.",
+                "Slot": "Hat", "Restrict": ["Hat"], "Color": "black", "Price": 60, "Style": "LEGENDARY", "Type" : "ACCESSORY",
+                "WearEffect": {"Femininity": [0, 10, 80]}, "Category" : [ "Gothic Lolita" ]
+            },
+            "gothic Collar": { // 9
+                "Name": "Black gothic collar", "ShortDesc": " a {COLOR} ribbon-lined leather collar emblazoned with the word 'SLAVE' burned into it.",
+                "LongDesc": "A snug leather collar lined on both sides in black lace, slave is burned into the leather to remind you exactly what you are..",
+                "Slot": "Neck", "Restrict": ["Neck"], "Color": "black", "Price": 50, "Style": "RARE", "Type" : "ACCESSORY",
+                "WearEffect": {"Femininity": [0, 10, 70]}, "Category" : [ "Gothic Lolita" ]
+            },
+            "gothic black bra": { // 10
+                "Name": "black bra", "ShortDesc": "a {COLOR} lace gothic style bra",
+                "LongDesc": "A sexy black lace gothic style bra.",
+                "Slot": "Bra", "Restrict": ["Bra"], "Color": "black", "Price": 80, "Style": "UNCOMMON", "Type" : "CLOTHING",
+                "WearEffect": {"Femininity": [0, 10, 60]}, "Category" : [ "Gothic Lolita", "BIMBO", "Sexy Dancer" ]
+            },
+            "black panties": { // 10
+                "Name": "black panties", "ShortDesc": "a pair of {COLOR} black panties",
+                "LongDesc": "Black sexy silk panties just asking for someone to remove them.",
+                "Slot": "Panty", "Restrict": ["Panty"], "Color": "black", "Price": 80, "Style": "UNCOMMON", "Type" : "CLOTHING",
+                "WearEffect": {"Femininity": [0, 10, 60]}, "Category" : [  "Gothic Lolita", "BIMBO", "Sexy Dancer" ]
+            },
+            "gothic frilly bloomers": { // 15
+                "Name": "frilly bloomers", "ShortDesc": "a pair of frilly {COLOR} cotton bloomers",
+                "LongDesc": "These bloomers are cut short and have a frilly bottom.",
+                "Slot": "Panty", "Restrict": ["Panty"], "Color": "black", "Price": 150, "Style": "RARE", "Type" : "CLOTHING",
+                "WearEffect": {"Femininity": [0, 10, 70]}, "Category" : [ "Gothic Lolita" ]
+            },
+            "gothic striped stockings": { // 15
+                "Name": "black stockings", "ShortDesc": "a pair of {COLOR} and purple striped stockings",
+                "LongDesc": "These stockings come up over your knees. They are striped, black and purple, with a slight lacey top that is adorned with tiny black roses.",
+                "Slot": "Stockings", "Restrict": ["Stockings"], "Color": "black", "Price": 75, "Style": "RARE", "Type" : "CLOTHING",
+                "WearEffect": {"Femininity": [0, 10, 60]}, "Category" : [ "Gothic Lolita" ]
+            },
+            "gothic frilly dress": { // 40
+                "Name": "black gothic dress", "ShortDesc": "a {COLOR} gothic dress",
+                "LongDesc": "A short sleeved snug black knee length dress. Lace along the the collar, sleeves and along the bottom of the heavily petticoated skirt that bounces with every step you take.",
+                "Slot": "Dress", "Restrict": ["Shirt", "Pants", "Dress", "Costume"], "Color": "black", "Price": 250, "Style": "LEGENDARY", "Type" : "ONE PIECE",
+                "WearEffect": {"Femininity": [0, 10, 70]}, "Category" : [ "Gothic Lolita" ]
+            },
+            "black boots": { // 15
+                "Name": "black high heeled boots", "ShortDesc": "A pair of black knee length heeled boots",
+                "LongDesc": "A pair of knee high black boots with a 5 inch heel and 1 inch platform. Red stars dangle from the zipper.",
+                "Slot": "Shoes", "Restrict": ["Shoes"], "Color": "black", "Price": 125, "Style": "RARE", "Type" : "CLOTHING",
+                "WearEffect": {"Femininity": [0, 5, 50]}, "Category" : [ "Gothic Lolita", "Sexy Dancer" ]
+            },
+            "black platform mary janes": { // 15
+                "Name": "black platform mary janes", "ShortDesc": "a pair of {COLOR} platform mary janes",
+                "LongDesc": "These black platform shoes have Have red stars for the buckles at the ankles and a chunky platform heel. Difficult to walk in but sexy.",
+                "Slot": "Shoes", "Restrict": ["Shoes"], "Color": "black", "Price": 200, "Style": "RARE", "Type" : "CLOTHING",
+                "WearEffect": {"Femininity": [0, 10, 70]}, "Category" : [ "Gothic Lolita" ]
+            },
 // Other Styles
             "cheap wig": {
                 "Name": "cheap wig", "ShortDesc": "a short {COLOR} wig",
