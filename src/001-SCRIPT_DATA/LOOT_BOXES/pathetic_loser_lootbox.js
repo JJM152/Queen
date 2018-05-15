bd0a8790d8bec812ca52e6a66fdbcbc76f9d1fcd
Aurelio <panslothdagmail.com>
2018-05-15 17:30:50 +0200

    tweaked as requested

---
+++ src/001-SCRIPT_DATA/LOOT_BOXES/pathetic_loser_lootbox.js
@@ -0,0 +1,65 @@
+App = App || { Data: { }, Entity: { } };
+App.Data.LootBoxes = App.Data.LootBoxes || { };
+App.Data.LootTables = App.Data.LootTables || { };
+
+// BOX TYPE     MINIMUM     BONUS ROLL  COLOR
+// COMMON       0           0           grey
+// UNCOMMON     20          10          lime
+// RARE         30          20          cyan
+// LEGENDARY    50          30          orange
+
+App.Data.LootBoxes["pathetic loser chest"] = {
+    "Name": "Pathetic loser chest",
+    "ShortDesc": "@@color:purple;A big box with a big purple bow over it@@",
+    "LongDesc": "This sparkling big box is shaped like a giftbox you used to give to your fiancee and opens with a big bow on it's top. Wonder what's inside?",
+    "Message": "With unrestrained curiosity you tear open the treasure chest to find...\n\n",
+    "Type": "LOOT_BOX",
+    "Charges": 1,
+    // Effect : [ TABLE, Minimum Roll, Bonus to roll
+    "UseEffect": { "LootBox": ["PATHETIC_LOSER", 0, 0]},
+    "Price": 0
+};
+
+App.Data.LootTables["PATHETIC_LOSER"] =  {
+    10 :  {
+        "LOOT" : [
+            { "TYPE" : "COSMETICS", "TAG" : "basic makeup", "QTY" : 20},
+            { "TYPE" : "COSMETICS", "TAG" : "hair accessories", "QTY" : 20},
+            { "TYPE" : "COSMETICS", "TAG" : "expensive makeup", "QTY" : 10},
+            { "TYPE" : "COSMETICS", "TAG" : "hair products", "QTY" : 10},
+            { "TYPE" : "FOOD", "TAG" : "bread crust", "QTY" : 15},
+            { "TYPE" : "FOOD", "TAG" : "mystery stew", "QTY" : 15},
+            { "TYPE" : "FOOD", "TAG" : "roast fish", "QTY" : 5},
+            { "TYPE" : "FOOD", "TAG" : "red plum", "QTY" : 5},
+            { "TYPE" : "FOOD", "TAG" : "cheap wine", "QTY" : 5},
+            { "TYPE" : "FOOD", "TAG" : "smugglers ale", "QTY" : 5},
+            { "TYPE" : "DRUGS", "TAG" : "medicinal herbs", "QTY" : 5},
+            { "TYPE" : "DRUGS", "TAG" : "healing potion", "QTY" : 5},
+            { "TYPE" : "DRUGS", "TAG" : "pixie dust", "QTY" : 10},
+            { "TYPE" : "CLOTHES", "TAG" : "wig", "QTY" : 1},
+            { "TYPE" : "CLOTHES", "TAG" : "choker", "QTY" : 1},
+            { "TYPE" : "CLOTHES", "TAG" : "fishnet tights", "QTY" : 1},
+            { "TYPE" : "CLOTHES", "TAG" : "pathetic loser costume", "QTY" : 1}
+
+        ] },
+    100 :  {
+        "LOOT" : [
+            { "TYPE" : "COSMETICS", "TAG" : "basic makeup", "QTY" : 20},
+            { "TYPE" : "COSMETICS", "TAG" : "hair accessories", "QTY" : 20},
+            { "TYPE" : "COSMETICS", "TAG" : "expensive makeup", "QTY" : 10},
+            { "TYPE" : "COSMETICS", "TAG" : "hair products", "QTY" : 10},
+            { "TYPE" : "FOOD", "TAG" : "bread crust", "QTY" : 15},
+            { "TYPE" : "FOOD", "TAG" : "mystery stew", "QTY" : 15},
+            { "TYPE" : "FOOD", "TAG" : "roast fish", "QTY" : 5},
+            { "TYPE" : "FOOD", "TAG" : "red plum", "QTY" : 5},
+            { "TYPE" : "FOOD", "TAG" : "cheap wine", "QTY" : 5},
+            { "TYPE" : "FOOD", "TAG" : "smugglers ale", "QTY" : 5},
+            { "TYPE" : "DRUGS", "TAG" : "medicinal herbs", "QTY" : 5},
+            { "TYPE" : "DRUGS", "TAG" : "healing potion", "QTY" : 5},
+            { "TYPE" : "DRUGS", "TAG" : "pixie dust", "QTY" : 10},
+            { "TYPE" : "CLOTHES", "TAG" : "wig", "QTY" : 1},
+            { "TYPE" : "CLOTHES", "TAG" : "choker", "QTY" : 1},
+            { "TYPE" : "CLOTHES", "TAG" : "fishnet tights", "QTY" : 1},
+            { "TYPE" : "CLOTHES", "TAG" : "pathetic loser costume", "QTY" : 1}
+        ] }        
+};
