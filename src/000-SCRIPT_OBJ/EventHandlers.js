d37df071d792345d820020b86e2dee34665509ef
JJM152 <jjm152@hotmail.com>
2018-05-20 14:16:17 +1000

    Global end states for no health and low will power. Added event for not keeping pirate crew happy

--- src/000-SCRIPT_OBJ/EventHandlers.js
+++ src/000-SCRIPT_OBJ/EventHandlers.js
@@ -1,21 +1,33 @@
-/**
- * Load some simple event handlers.
- * @type {{Init: App.EventHandlers.Init}}
- * */
-App.EventHandlers = {
-  Init: function() {
+
+App.EventHandlers = new function() {
+
+    this.Init = function() {
+
       console.log("Initializing Event Handlers...");
+
       // Add a hyperlink to run back to your Cabin when it's very, very late.
-      $(document).on(':passagerender', function (ev) {
-          if (typeof SugarCube.State !== 'undefined') {
-              if (typeof SugarCube.State.variables !== 'undefined') {
-                  if (typeof SugarCube.State.variables.Player !== 'undefined' ) {
-                      if (SugarCube.State.variables.Player.Phase >= 4) {
-                          $(ev.content).wiki("<<lateNightTeleport>>");
-                      }
+          $(document).on(':passagerender', function (ev) {
+              if (App.EventHandlers.exists("SugarCube.State.variables.Player")) {
+                  if (SugarCube.State.variables.Player.Phase >= 4) {
+                      $(ev.content).wiki("<<lateNightTeleport>>");
                   }
               }
-          }
-      });
-  }
+          });
+
+      // Do something on passage start
+      //      $(document).on(':passagestart', function (ev) {
+      //          if (ev.passage['tags'].includes("event")) {
+      //              console.log("event tag detected");
+      //          }
+      //
+      //      });
+    };
+
+    this.exists = function(namespace)
+    {
+        var tokens = namespace.split('.');
+        return tokens.reduce(function(prev, curr) {
+            return (typeof prev == "undefined") ? prev : prev[curr];
+        }, window);
+    };
 };
