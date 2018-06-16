962894514f388ad466c0b20467e40907edd1beb3
JJM <jjm152@hotmail.com>
2018-06-17 03:06:36 +1000

    Added new areas to Abamond.

--- src/001-SCRIPT_DATA/NPCS/abamond_npcs.js
+++ src/001-SCRIPT_DATA/NPCS/abamond_npcs.js
@@ -12,3 +12,12 @@ App.Data.NPCS["Paola"] = {
         "LongDesc": "This intimidatingly tall black woman is dressed in an odd assortment of coloured cloth and garish jewelry. Her pendulous breasts jiggle and shake in their attempt to free themselves from the confines of her loose tube-top whenever she breaks out into a hearty laugh."
 };
 
+App.Data.NPCS["Papa Baba"] = {
+    "Name": "Papa Baba",
+    "Mood": 40,
+    "DailyMood": 0,
+    "Lust": 40,
+    "DailyLust": 10,
+    "Title": "NPC_NAME Houngan",
+    "LongDesc": "This lean, dusky voodoo priest is dressed in a threadbare black coat with stained, off-white breeches and shoeless feet. On his head is an old capotain hat adorned with a spray of feathers and a crudely-painted death's head. He also wears a necklace of alternating beads and crocodile teeth, a hemp cord bracelet with a carved, wooden charm, and an ivory-colored sash tied around his waist. His solemn stare is slightly unsettling, his eyes distant as if he is seeing the spirit and material worlds all at once."
+};
