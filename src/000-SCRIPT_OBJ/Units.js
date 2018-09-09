65f64fdd5ec4d4735750b06847b54b1cf8929632
ezsh <43062025+ezsh@users.noreply.github.com>
2018-09-09 22:37:51 +0200

    Add bra cup sizes to the units system.
    The cups sizes are based on bust and underbust difference in cm. The
    imperial system uses UK cup sizes, the metric one uses the EU sizing.

--- src/000-SCRIPT_OBJ/Units.js
+++ src/000-SCRIPT_OBJ/Units.js
@@ -11,7 +11,37 @@ App.UnitSystem = function () {
 		lengthUnits       : ["centimeter", "meter", "kilometer"],
 		lengthUnitsPlural : ["centimetres", "meters", "kilometers"],
 		lengthScaleFactors: [100, 1000],
-		lengthSymbolDelim : '&thinsp;'
+		lengthSymbolDelim : '&thinsp;',
+		braCups: { // difference in cm, taken from http://brasizecalculator.eu/ for underbust of  80 cm
+			 0 : "Flat",
+			 5 : "AA",
+			 7 : "A",
+			 9 : "B",
+			11 : "C",
+			13 : "D",
+			15 : "E",
+			17 : "F",
+			19 : "G",
+			21 : "H",
+			23 : "I",
+			25 : "J",
+			27 : "K",
+			29 : "L",
+			31 : "M",
+			33 : "N",
+			35 : "O",
+			37 : "P",
+			39 : "Q",
+			41 : "R",
+			43 : "S",
+			45 : "T",
+			47 : "U",
+			49 : "V",
+			51 : "W",
+			53 : "X",
+			55 : "Y",
+			57 : "Z"
+		}
 	};
 
 	this.imperialSystemDefinition = {
@@ -20,7 +50,57 @@ App.UnitSystem = function () {
 		lengthUnits       : ["inch", "foot", "mile"],
 		lengthUnitsPlural : ["inches", "feet", "miles"],
 		lengthScaleFactors: [12, 5280],
-		lengthSymbolDelim : ''
+		lengthSymbolDelim : '',
+		braCups: { // difference in inches, taken from http://brasizecalculator.eu/
+			 0 : "Flat",
+			 1 : "AA",
+			 2 : "A",
+			 3 : "B",
+			 4 : "C",
+			 5 : "D",
+			 6 : "DD",
+			 7 : "E",
+			 8 : "F",
+			 9 : "FF",
+			10 : "G",
+			11 : "GG",
+			12 : "H",
+			13 : "HH",
+			14 : "J",
+			15 : "JJ",
+			16 : "K",
+			17 : "KK",
+			18 : "L",
+			19 : "LL",
+			20 : "M",
+			21 : "MM",
+			22 : "N",
+			23 : "NN",
+			24 : "O",
+			25 : "OO",
+			26 : "P",
+			27 : "PP",
+			28 : "Q",
+			29 : "QQ",
+			30 : "R",
+			31 : "RR",
+			32 : "S",
+			33 : "SS",
+			34 : "T",
+			35 : "TT",
+			36 : "U",
+			37 : "UU",
+			38 : "V",
+			39 : "VV",
+			40 : "W",
+			41 : "WW",
+			42 : "X",
+			43 : "XX",
+			44 : "Y",
+			45 : "YY",
+			46 : "Z",
+			47 : "ZZ"
+		}
 	};
 
 // 	this.system = {};
@@ -53,6 +133,17 @@ App.UnitSystem = function () {
 		}
 		res += cmpts[0] + this.system.lengthSymbolDelim + this.system.lengthSymbols[0];
 		return res;
+	};
+
+	this.cupString = function(bustCM, underbustCM) {
+		var diff = this.lengthValue(bustCM - underbustCM);
+		var cups = this.system.braCups;
+		for (var cup in cups) {
+			if (!cups.hasOwnProperty(cup)) continue;
+
+			if (diff<= cup) return cups[cup];
+		}
+		return "OutOfRangeCup: " + diff;
 	};
 };
 
