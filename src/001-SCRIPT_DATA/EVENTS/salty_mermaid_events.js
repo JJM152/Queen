39eaf1f4485871bdb7932212366191256fcd8de2
JJM152 <jjm152@hotmail.com>
2019-02-16 02:34:18 +1000

    added penis shrink event

--- src/001-SCRIPT_DATA/EVENTS/salty_mermaid_events.js
+++ src/001-SCRIPT_DATA/EVENTS/salty_mermaid_events.js
@@ -15,5 +15,18 @@ App.Data.Events["Cabin"] = [
         CHECK: function(p) { // Condition function that is called to check if the event fires.
             return ((p.GetNPC("Crew").Mood() < 40) && (Math.floor(Math.random() * 4) == 0));
         }
+    },
+    { 
+        ID: 'PenisShrink', // A unique ID for the event.
+        FROM: 'Sleep', // The passage you are traversing from
+        MAX_REPEAT: 1, // Number of times the event can repeat, 0 means forever.
+        MIN_DAY: 1, // Minimum day the event shows up on
+        MAX_DAY: 0, // Maximum day the event shows up on, 0 means forever
+        COOL: 0, // Interval between repeats on this event.
+        PHASE: [ 0 ], // Time phases the event is valid for
+        PASSAGE: 'PenisShrinkEvent', // Override passage that the player is routed to. 
+        CHECK: function(p) { // Condition function that is called to check if the event fires.
+            return (p.GetStat("BODY", "Penis") <= 34 && p.GetStat("BODY", "Balls") <= 34);
+        }
     }
 ];
