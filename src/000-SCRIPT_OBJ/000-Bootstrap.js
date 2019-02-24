198a4b4cc022a48d4198199f0959dbe68538a74d
ezsh <43062025+ezsh@users.noreply.github.com>
2019-02-24 13:18:44 +0100

    Reverse declaration order in the bootstrap file
    ...to make the order more logical.

--- src/000-SCRIPT_OBJ/000-Bootstrap.js
+++ src/000-SCRIPT_OBJ/000-Bootstrap.js
@@ -1,26 +1,24 @@
-window.App = {};
-
-// the same declaration for code parsers (IDEs) that don't like the line above
-var App = window.App || {};
-
-// namespace definitions
-App.Data = {
-	Clothes: {},
-	EffectLib: {},
-	Events: {},
-	Drugs: {},
-	Food: {},
-	JobData: {},
-	Loot: {},
-	LootBoxes: {},
-	LootTables: {},
-	Misc: {},
-	NPCS: {},
-	Quests: {},
-	Slots: {},
-	Stores: {},
-	Tarot: {},
-	Whoring: {},
+var App = {
+	Data: {
+		Clothes: {},
+		EffectLib: {},
+		Events: {},
+		Drugs: {},
+		Food: {},
+		JobData: {},
+		Loot: {},
+		LootBoxes: {},
+		LootTables: {},
+		Misc: {},
+		NPCS: {},
+		Quests: {},
+		Slots: {},
+		Stores: {},
+		Tarot: {},
+		Whoring: {},
+	},
+	Entity: {}
 };
 
-App.Entity = {};
+// export the App object from the SugarCube closured eval()
+window.App = App;
