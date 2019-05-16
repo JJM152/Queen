e788b3d8d71c3a07aa3f9302b1e53f7d9ce70c6f
JJM152 <jjm152@hotmail.com>
2019-05-16 10:27:42 +1000

    Patch 1 Stat tracking for new skills Fixed loot payout scaling on jobs Fixed wrong condition in uticus chat in room transition updated changelog

--- src/000-SCRIPT_OBJ/Tasks.js
+++ src/000-SCRIPT_OBJ/Tasks.js
@@ -896,7 +896,10 @@ App.Scene = class Scene {
         Results[Check.TAG] = r;
         this._Results[Check.TAG] = r;
         if (typeof Check.REWARD !== 'undefined') {
-            this._CalculateReward(Check.REWARD, Check.R_NAME, Value, Check.OPT, Result);
+            // automatically scale values on check lines that do not have options.
+            Value = (typeof Check.OPT === 'undefined' || (typeof Check.OPT === 'number' && Check.OPT == 0)) ?
+                Result : Value;
+            this._CalculateReward(Check.REWARD, Check.R_NAME, Value, Check.OPT, Results);
         }
     }
 
