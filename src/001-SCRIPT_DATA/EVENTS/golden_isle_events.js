31317ae023510d0e7daf9ac9d42df0cc763bb25a
JJM152 <jjm152@hotmail.com>
2019-07-10 01:41:21 +1000

    bertie quest part 3 event quest item map framework for quest entry

--- src/001-SCRIPT_DATA/EVENTS/golden_isle_events.js
+++ src/001-SCRIPT_DATA/EVENTS/golden_isle_events.js
@@ -49,7 +49,26 @@ App.Data.Events["GI_GovernorsMansionInside"] = [
                return t <= p.Day;
            }
            return false;
-
         }
+    },
+];
+
+App.Data.Events["GoldenIsle"] = [
+    {
+        ID: 'BertieCourierEvent',
+        FROM: 'Any',
+        MAX_REPEAT: 0,
+        MIN_DAY: 0,
+        MAX_DAY: 0,
+        COOL: 5,
+        PHASE: [ 0, 1, 2, 3, 4],
+        PASSAGE: 'BertieCourierEvent',
+        CHECK: function(p) {
+            if (p.QuestFlags.hasOwnProperty('BERTIE_QUEEN_PT2_CompletedOn')) {
+                let t = p.QuestFlags['BERTIE_QUEEN_PT2_CompletedOn'] + 5;
+                return t <= p.Day;
+            }
+            return false;
+         }
     },
 ];
